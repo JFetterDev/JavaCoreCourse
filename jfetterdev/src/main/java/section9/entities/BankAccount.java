2f075324b9a45b37a66780f390bb69223d41ebbc
Julio Fetter <JFetterdev@gmail.com>
2023-08-27 12:30:52 -0300

    First Commit

---
+++ jfetterdev/src/main/java/section9/entities/BankAccount.java
@@ -0,0 +1,45 @@
+package section9.entities;
+
+public class BankAccount {
+    private int number;
+    private String holder;
+    private double balance;
+
+    public BankAccount(int number, String holder, double initialDeposit) {
+        this.holder = holder;
+        this.number = number;
+        deposit(initialDeposit);
+    }
+
+    public BankAccount(int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+    public void withdraw(double amount) {
+        balance -= amount + 5.00;
+    }
+
+    public String toString() {
+        return ("Account " + number + ", Holder: " + holder + ", Balance: $" + String.format("%.2f", balance));
+    }
+}
